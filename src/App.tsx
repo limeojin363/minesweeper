dfd90b07079346a50c4d07d0ba9da0edd8dead64
Limeojin <2002eojin@gmail.com>
2024-12-04 11:47:42 +0000

    fix: 모바일 longpress 시 반응 차단

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import styled from "@emotion/styled";
 import Board from "./components/board/Board";
 
 const App = () => {
-  return <Board />;
+  return (
+    <BoardWrapper>
+      <Board />
+    </BoardWrapper>
+  );
 };
 
 export default App;
+
+const BoardWrapper = styled.div`
+  user-select: none;
+  -webkit-user-select: none;
+`;
